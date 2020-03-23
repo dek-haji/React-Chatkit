a4061447a34d485c662ba9119f4c8a815d15afd9
dek-haji <deeeqhaji@gmail.com>
2020-03-23 18:07:09 -0500

    imported Charkit from the pusher client

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import Chatkit from "@pusher/chatkit-client"
 import MessageList from "./components/MessageList"
 import SendMessageForm from "./components/SendMessageForm"
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
+
 
 class App extends React.Component {
     render() {
